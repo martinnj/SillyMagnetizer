ad4bc6555f4bbcbd3792691b3cd7bfe3317edaff
Martin Jørgensen <hello@martinnj.dk>
2018-07-28 14:19:13 +0200

    Add registry keys to make Silly Magnetizer the default application for magnet links.

--- SillyMagentizer/Properties/AssemblyInfo.cs
+++ SillyMagentizer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SillyMagentizer")]
+[assembly: AssemblyTitle("Silly Magentizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SillyMagentizer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Silly Magentizer")]
+[assembly: AssemblyCopyright("Copyright © Martin Jørgensen 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
